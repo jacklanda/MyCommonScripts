90f957ab586c4606c35462e7472bc3069aa1bfa3
jacklanda <yonyonlau@gmail.com>
2021-01-24 22:05:19 +0800

    update batch_download

--- batch_download.sh
+++ batch_download.sh
@@ -1,11 +1,4 @@
 #! /bin/zsh
-# the input_file's format like below,
-# split with punc ':' between website name
-# and website target url:
-#
-# google: https://google.com
-# amazon: https://www.amazon.com
-# ...
 
 if [ $# -lt 1 ]; then
     echo -e "\nusage: sh wget_batch_download.sh [input_batch_url_file]\n"
@@ -13,18 +6,11 @@ if [ $# -lt 1 ]; then
 fi
 
 input_file=$1
-#SEND_THREAD_NUM=30
-#tmp_fifofile="/tmp/$$.fifo"
-#mkfifo "$tmp_fifofile"
-#exec 6<>"$tmp_fifofile"
-#for ((i=0;i<$SEND_THREAD_NUM;i++))
-#do
-    #echo
-#done >&6
 
 DirOutput="output"
 mkdir $DirOutput
 cd $DirOutput
+
 while read line
 do
     item_name=`echo $line | awk -F ':' '{print $1}'`
